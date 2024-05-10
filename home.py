ad158aef5172d8182df45d94fa8821fc001fef8f
agjin9613 <agjin9613@gmail.com>
2024-05-10 13:26:16 +0900

    메인화면 수정

--- home.py
+++ home.py
@@ -5,8 +5,8 @@ def run_home():
     st.markdown("데이터 출처 - [공공데이터포털:인천광역시_정류장별 이용승객 현황](https://www.data.go.kr/data/15048264/fileData.do)")
 
     st.image('./image/인천_시내버스_수소버스_전자신문.jpg', use_column_width=True)
-    st.text('인천버스 정류장별 이용현황(카드 이용, 현금 이용)을 알 수 있는 앱이며,')
-    st.text('머신러닝을 이용해 트래픽도 예측해볼 수 있습니다.')
+    st.text('인천버스 정류장별 이용현황(카드 이용, 현금 이용)을 알 수 있습니다.')
+
 
 if __name__ == "__main__":
     run_home()
--- ml.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def run_ml():
-    
-    if __name__ == "__main__":
-        run_ml()
